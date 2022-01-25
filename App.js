6a13fea5c87032856d0e4316bd8563e1ff04f23c
chicha11 <75187259+chicha11@users.noreply.github.com>
2022-01-25 15:29:25 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Button,Image} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    count: 0
+  }
+  incrementValue = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  decrementValue = () => {
+    this.setState({count: this.state.count - 1})
+  }
+  render() {
+    return (
+     
+      <View style={styles.container}>
+       <Image style={styles.lion} source={require('./imgs/lion.png')} />
+        <Text style={styles.welcome}>Kitna Pyaar krte ho Mujhse</Text>
+        <Text style={styles.count}>{this.state.count}</Text>
+        <Button 
+        style={styles.btn}
+        title="Increase"
+        onPress={this.incrementValue}
+        />
+        <View style={{padding: 30}} />
+        <Button 
+        style={styles.btn}
+        title="Decrease"
+        onPress={this.decrementValue}
+        />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'#ECB390',
+    alignItems:'center',
+    justifyContent:'center',
   },
+  welcome:{
+    fontSize:32,
+    padding:30,
+    fontWeight: 'bold',
+    color:'white'
+    
+  },
+
+  count : {
+    fontSize: 40,
+    marginBottom:30
+  },
+
+  lion:{
+    height:400,
+    width:350
+  },
+  btn:{
+    borderRadius:40
+  }
 });
